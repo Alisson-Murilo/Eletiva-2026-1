8ad82e05d956aa883f2c83060b0be274473bc16f
Alisson Murilo <alisson-murilo2011@hotmail.com>
2026-02-27 15:20:50 -0300

    Fiz o exercício 8

--- Aula 24-02/exercicio 8.php
+++ Aula 24-02/exercicio 8.php
@@ -4,31 +4,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 3</title>
+    <title>Exercício 8</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercício 3</h1>
+        <h1>Exercício 8</h1>
         <form method="post">
             <div class="mb-3">
-                <label for="valor1" class="form-label">Informe o primeiro valor:</label>
-                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+                <label for="largura" class="form-label">Insira a largura de um retângulo: </label>
+                <input type="number" id="largura" name="largura" class="form-control" required="">
             </div>
             <div class="mb-3">
-                <label for="valor2" class="form-label">Informe o segundo valor: </label>
-                <input type="number" id="valor2" name="valor2" class="form-control" required="">
+                <label for="altura" class="form-label">Insira a altura de um retângulo: </label>
+                <input type="text" id="altura" name="altura" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
         <?php
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                $valor1 = $_POST['valor1'];
-                $valor2 = $_POST['valor2'];
-                $resultado = $valor1 * $valor2;
-                echo "A multiplicação é: $resultado";
-        }
+                $largura = $_POST['largura'];
+                $altura = $_POST['altura'];
+                $area = $largura * $altura;
+                echo "A área do retagulo é: $area ";
+            }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
