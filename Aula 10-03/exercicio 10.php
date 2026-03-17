d8b5db7a1245ec0ade23d3d72ebef0e8c07421f4
Alisson Murilo <alisson-murilo2011@hotmail.com>
2026-03-17 14:51:04 -0300

    Fiz o exercício 10

--- Aula 10-03/exercicio 10.php
+++ Aula 10-03/exercicio 10.php
@@ -21,7 +21,14 @@
         <?php
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 $nome = $_POST['nome'];
-                
+                $saida = "Saída -> ";
+                $partes = explode(" ", $nome);
+                for($i = 0; $i < count($partes); $i++){
+                    if (ctype_upper(substr($partes[$i], 0, 1))){
+                        $saida .= substr($partes[$i], 0, 1) . ".";
+                    } 
+                }
+                echo "<p>$saida</p>";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
